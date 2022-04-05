a55660055407391f093a1b3dd11776dbd00b606a
Demidenko Yuriy <dimidengo@yandex.ru>
2022-04-05 12:58:16 +0300

    Initial commit

---
+++ netology_ib_instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netology_ib_instruments
+//
+//  Created by Юрий Демиденко on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
